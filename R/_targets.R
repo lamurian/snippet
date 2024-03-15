edb7131e33b397072f9c8d5a56bc4c1242e44564
lamurian <lamuri.aly8@gmail.com>
2024-03-15 14:12:24 +0100

    Add snippet for R targets pipeline

---
+++ R/_targets.R
@@ -0,0 +1,24 @@
+# Load packages
+pkgs <- c("magrittr", "targets", "tarchetypes", "crew")
+pkgs_load <- sapply(pkgs, library, character.only = TRUE)
+
+# Source user-defined functions
+funs <- list.files("src/R", pattern = "*.R", full.name = TRUE) %>%
+  lapply(source)
+
+# Set option for targets
+tar_option_set(
+  packages   = pkgs,
+  error      = "continue",
+  memory     = "transient",
+  controller = crew_controller_local(worker = 4),
+  storage    = "worker",
+  retrieval  = "worker",
+  garbage_collection = TRUE
+)
+
+seed <- 1810
+
+# Set paths for the raw data
+tbls <- list.files("data/raw", full.names = TRUE, pattern = "*csv")
+
